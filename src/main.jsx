56a7ebd80fc1929bf6356afbd74e57de9093dad1
songsu <jole_wong@outlook.com>
2023-12-16 15:42:30 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import '@/assets/css/cover.less'
-
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
